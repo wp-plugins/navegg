40c715c576047b8e715fe8080b74048079b3dff6
navegg <navegg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-29 19:09:14 +0000

    Current version. 3.0.
    git-svn-id: https://plugins.svn.wordpress.org/navegg/trunk@975428 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ contents/cssInit.php
@@ -0,0 +1,75 @@
+<style type="text/css" media="screen">
+.toplevel-navegg .ic_nvg{ background:transparent url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat right -231px;}
+.inside{padding:10px!important;}
+.inside p{ line-height:20px; padding-bottom:10px; margin:5px 0 0 5px; }
+.nvg-menu-image{float:left; width:28px; height:28px; background:transparent url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat right -256px;}
+
+.rep_nvg { float:left; width:49%; margin:0 5px;}
+.rep_nvg div{ background:#FFFBCC url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat -637px -103px; height:80px; border:1px solid #E6DB55; width:98%; min-width:550px; max-width: 782px;}
+.rep_nvg div p{ padding:15px 15px 0 181px;}
+.rep_nvg div p a{ color:#60A51B; font-weight:bold;}
+.rep_nvg div p a:hover{ color:#D54E21;}
+
+#icon-navegg{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat right top;}
+
+.nvg_lst{ margin:0;}
+.nvg_lst li.tlt{ float:none; /* color:#21759B; */ padding:0 0 10px 0; width:auto;}
+.nvg_lst li{ float:left; padding:2px 0 2px 22px; font-weight:bold; min-width:120px;}
+.nvg_lst li.d{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat 1px -14px; margin-bottom:8px;}
+		.nvg_lst li.d:hover{background-position:1px -36px;}
+.nvg_lst li.s{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat 1px -104px; margin-bottom:8px;}
+		.nvg_lst li.s:hover{background-position:1px -126px;}
+.nvg_lst li.b{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat left -193px; margin-bottom:8px;}
+		.nvg_lst li.b:hover{background-position:left -215px;}
+.nvg_lst li.f{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat 3px -59px; margin-bottom:8px; clear:left;}
+		.nvg_lst li.f:hover{background-position:3px -81px;}
+.nvg_lst li.p{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat left -149px; margin-bottom:8px;}
+		.nvg_lst li.p:hover{background-position:left -171px;}
+.nvg_lst li.c{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat left -241px; margin-bottom:8px;}
+		.nvg_lst li.c:hover{background-position:left -263px;}
+.nvg_lst li.h{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat 3px -59px; float:none; margin-bottom:5px;}
+		.nvg_lst li.h:hover{background-position:3px -81px;}
+.nvg_lst li.r{ background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat left -288px; float:none;}
+		.nvg_lst li.r:hover{background-position:left -310px;}
+/*.nvg_lst li.r span{ color:#60a51b;}
+.nvg_lst li.r span:hover{ color:#D54E21;}*/
+
+.postbox-container a { color: #63969f; }
+.postbox-container a:hover { color: #4a737b; }
+
+#dashboard-widgets-wrap{ min-width:950px;}
+#dashboard-widgets h4{font-size:16px; margin: 0 0 0 5px}
+#dashboard-widgets a{margin-left:5px; vertical-align:baseline;}
+#dashboard_right_now ul.nvg_lst li a:before{display:none;}
+
+.postbox h3{ cursor:auto!important;}
+.postbox h3 .on{ color:#3270fb; font-size:11px; float:right; padding:2px 0 2px 0; text-align:left;}
+.postbox h3 .on span{background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat -785px -336px; display:block; float:left; width:12px; height:16px; margin-top:-2px; margin-right:6px;}
+
+.postbox h3 .off{ color:#9c9a9a; font-size:11px; float:right; padding:2px 0 2px 0; text-align:left;}
+.postbox h3 .off span{background:url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat -785px -365px; display:block; float:left; width:12px; height:16px; margin-top:-2px; margin-right:6px;}
+
+.postbox-container .meta-box-sortables{ min-height:150px;}
+#dashboard-widgets .postbox-container{width:99%; display:block;}
+.clearme{ clear:both;}
+
+
+
+.id_nvg{ background:#fff url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat left bottom; min-height:250px; max-height:265px;  padding-bottom:10px; min-width:550px; max-width: 782px;}
+.id_nvg .inside, .cnt_nvg .inside, .pn_nvg .inside{ padding-left:178px!important;}
+.cnt_nvg { background: white url(<?php echo plugins_url( 'ui_wp_nvg_usr.png', __FILE__ ); ?>) no-repeat left bottom;  padding-bottom: 10px; }
+.cnt_nvg .inside{ padding-left:178px!important;}
+.id_nvg input, .cnt_nvg input { background:#FFF url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat 7px -329px; padding:3px 5px 3px 30px!important; margin:0 10px 15px 0; font-size:15px; width:131px; /* color:rgb(45, 148, 208);  border:1px solid #63a7cf;*/}
+.cnt_nvg input{margin-top:5px;}
+/* #idNvg { color:#2d94d0; border:1px solid #63a7cf; }
+div.postbox.id_nvg button.button, div.postbox.cnt_nvg button.button{height:36px!important; top: 4px; position: relative;}*/
+
+.pn_nvg { background: #F9F9F9 url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat -637px -59px; }
+
+.cnt_nvg, .pn_nvg { min-width: 550px; max-width: 782px; }
+
+.updtSucess{ padding:0 0 7px 23px!important; margin:0; color:#090; background:transparent url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat -783px -430px;}
+.updtFail{ padding:0 0 7px 23px!important; margin:0; color:#f82b33; background:transparent url(<?php echo plugins_url( 'ui_wp_nvg.png', __FILE__ ); ?>) no-repeat -783px -454px;}
+
+.inside .nvgerror { clear: both; margin-top: -5px; color: red; font-weight: bold; }
+</style>
